4b94ae738896032e57786fbd4c6b764efe3b8c5b
VladislavMiroshkin <v.miroshkin@a1qa.com>
2026-01-13 21:08:21 +0100

    correct username

--- src/test/java/tests/CreateUserTest.java
+++ src/test/java/tests/CreateUserTest.java
@@ -24,8 +24,8 @@ public class CreateUserTest {
     public void createUser() {
         User user = new User();
         user.setUsername(RandomUtils.getRandomAlphabeticString());
-        user.setFirstName(Vladislav);
-        user.setLastName(Miroshkin);
+        user.setFirstName("Vladislav");
+        user.setLastName("Miroshkin");
         user.setEmail(RandomUtils.getRandomAlphabeticString());
         user.setPassword(RandomUtils.getRandomAlphabeticString());
         user.setPhone(RandomUtils.getRandomAlphabeticString());
